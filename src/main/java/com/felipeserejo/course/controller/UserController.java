48e10fd8c9ea49e89c1c13ff588c4c39f02e5169
lipe-s-m <felipe.s.m1609@gmail.com>
2025-03-07 14:11:12 -0300

    :sparkles: Service layer, component registration

--- src/main/java/com/felipeserejo/course/controller/UserController.java
+++ src/main/java/com/felipeserejo/course/controller/UserController.java
@@ -1,8 +1,11 @@
 package com.felipeserejo.course.controller;
 
 import com.felipeserejo.course.domain.User;
+import com.felipeserejo.course.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,16 +16,21 @@ import java.util.List;
 @RequestMapping(value = "/users")
 public class UserController {
 
+    @Autowired
+    private UserService userService;
+
     @GetMapping
     public ResponseEntity<List<User>> findAllUsers(){
-        User user1 = new User(1, "Felipe", "emailFelipe@gmail.com", "senha1", "21998887766");
-        User user2 = new User(2, "João", "emailJoão@gmail.com", "123213", "21932347837");
-        User user3 = new User(3, "Maria", "emailMaria@gmail.com", "SOUuma.Senha", "21991112233");
-        List<User> users = new ArrayList<>();
-        users.add(user1);
-        users.add(user2);
-        users.add(user3);
-        return ResponseEntity.ok(users);
+
+        List<User> users = userService.findAll();
+
+        return ResponseEntity.ok().body(users);
+    }
+
+    @GetMapping(value = "/{id}")
+    public  ResponseEntity<User> findById(@PathVariable Long id){
+        User user = userService.findById(id);
+        return ResponseEntity.ok().body(user);
     }
 
 }
--- src/main/java/com/felipeserejo/course/repository/UserRepository.java
+++ src/main/java/com/felipeserejo/course/repository/UserRepository.java
@@ -3,7 +3,8 @@ package com.felipeserejo.course.repository;
 import com.felipeserejo.course.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+
 public interface UserRepository extends JpaRepository<User, Long> {
-    
+
 
 }
